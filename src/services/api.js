9aa03ed1a804b63c7357b5a1deae837586dee37e
Nicolas Renard <nicolas.renard@idt.digital>
2019-07-01 10:01:35 -0300

    :hammer: Add .env.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://www.googleapis.com/books/v1/volumes',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.response.use(
